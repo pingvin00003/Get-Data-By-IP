53fa08f30b566e4d2acecc19a973dd4646b37381
ice <58382156+pingvin00003@users.noreply.github.com>
2023-02-26 12:38:16 +0300

    Add files via upload

---
+++ dataByIP/dataByIP/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace dataByIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputIPTextBox = new System.Windows.Forms.TextBox();
+            this.GetDataByIpButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DataLabel = new System.Windows.Forms.Label();
+            this.InfoButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputIPTextBox
+            // 
+            this.InputIPTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InputIPTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputIPTextBox.Location = new System.Drawing.Point(12, 45);
+            this.InputIPTextBox.Multiline = true;
+            this.InputIPTextBox.Name = "InputIPTextBox";
+            this.InputIPTextBox.Size = new System.Drawing.Size(144, 23);
+            this.InputIPTextBox.TabIndex = 0;
+            // 
+            // GetDataByIpButton
+            // 
+            this.GetDataByIpButton.FlatAppearance.BorderSize = 0;
+            this.GetDataByIpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GetDataByIpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GetDataByIpButton.Location = new System.Drawing.Point(12, 74);
+            this.GetDataByIpButton.Name = "GetDataByIpButton";
+            this.GetDataByIpButton.Size = new System.Drawing.Size(81, 28);
+            this.GetDataByIpButton.TabIndex = 1;
+            this.GetDataByIpButton.Text = "Получить";
+            this.GetDataByIpButton.UseVisualStyleBackColor = true;
+            this.GetDataByIpButton.Click += new System.EventHandler(this.GetDataByIpButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // DataLabel
+            // 
+            this.DataLabel.AutoSize = true;
+            this.DataLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DataLabel.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DataLabel.Location = new System.Drawing.Point(13, 153);
+            this.DataLabel.Name = "DataLabel";
+            this.DataLabel.Size = new System.Drawing.Size(0, 23);
+            this.DataLabel.TabIndex = 3;
+            this.DataLabel.Click += new System.EventHandler(this.DataLabel_Click);
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.FlatAppearance.BorderSize = 0;
+            this.InfoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InfoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InfoButton.Location = new System.Drawing.Point(99, 74);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(57, 28);
+            this.InfoButton.TabIndex = 4;
+            this.InfoButton.Text = "Инфо";
+            this.InfoButton.UseVisualStyleBackColor = true;
+            this.InfoButton.Click += new System.EventHandler(this.InfoButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Данные:";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CloseButton.Location = new System.Drawing.Point(204, 486);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 28);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.Text = "Закрыть";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(291, 526);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.DataLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GetDataByIpButton);
+            this.Controls.Add(this.InputIPTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Узнать данные по IP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputIPTextBox;
+        private System.Windows.Forms.Button GetDataByIpButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DataLabel;
+        private System.Windows.Forms.Button InfoButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ dataByIP/dataByIP/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace dataByIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();    
+        }
+
+        // Получение IP и чтение данных с IP
+        private void GetDataByIpButton_Click(object sender, EventArgs e)
+        {
+            DataLabel.Text = "";
+            string line = "";
+            Match match;
+
+            // Проверка если в TextBox ничего нету то данные читаются по IP пользователя
+            if (InputIPTextBox == null)
+            {
+                // Читаем сайт чтобы получить IP пользователя
+                using (WebClient wc = new WebClient())
+                    line = wc.DownloadString($"http://free.ipwhois.io/xml/");
+
+                // Получаем IP пользователя
+                match = Regex.Match(line, "<ip>(.*?)</ip>");
+                InputIPTextBox.Text = match.Groups[1].Value;
+            }
+
+            // Читаем сайт чтобы в последствии получить данные пользователя по IP который узнали выше
+            using (WebClient wc = new WebClient())
+                line = wc.DownloadString($"https://ipwho.is/{InputIPTextBox.Text}");
+
+            // Тут и так все понятно
+            match = Regex.Match(line, "{\"ip\":\"(.*?)\",");
+            DataLabel.Text += match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"country\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"country_code\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"region\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"city\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"latitude\":(.*?),");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"longitude\":(.*?),");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"postal\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"org\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"isp\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"domain\":\"(.*?)\"},");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"utc\":\"(.*?)\",");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+
+            match = Regex.Match(line, "\"current_time\":\"(.*?)\"}");
+            DataLabel.Text += "\n" + match.Groups[1].Value;
+        }
+        
+        // Кнопка информаци
+        private void InfoButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Получить свой IP а так же данные по своему IP можно нажав на кнопку \"Получить\" без указания IP.", "Инфо", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Устанавливаем цвета для различных контролов при загрузки формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region DesignMainSettingsForm
+            BackColor = Color.FromArgb(19, 22, 27);
+            ForeColor = Color.White;
+            //ForeColor = Color.FromArgb(87, 181, 223);
+            #endregion
+
+            #region DesignButtons
+            GetDataByIpButton.BackColor = Color.FromArgb(41, 50, 57);
+            InfoButton.BackColor = Color.FromArgb(41, 50, 57);
+            CloseButton.BackColor = Color.FromArgb(41, 50, 57);
+            #endregion
+
+            #region DesignTextBox
+            InputIPTextBox.BackColor = Color.FromArgb(41, 50, 57);
+            InputIPTextBox.ForeColor = Color.White;
+            #endregion
+
+            #region DesignText
+            label1.ForeColor = Color.FromArgb(87, 181, 223);
+            label2.ForeColor = Color.FromArgb(87, 181, 223);
+            CloseButton.ForeColor = Color.FromArgb(87, 181, 223);
+            #endregion
+        }
+
+        // Кнопка закрытия программы
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // При наведении на данные которые получили, мы копируем их в буффер и выводим соответствующее сообщение
+        private void DataLabel_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(DataLabel.Text);
+            MessageBox.Show("Скопированно в буффер обмена !", "Инфо", MessageBoxButtons.OK);
+        }
+    }
+}
